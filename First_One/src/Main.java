676398d2b412ab497fa508110b94275db670aadd
Kunal Avinash Kanse <kunalkanse2006@gmail.com>
2026-01-10 17:36:58 +0530

    Add files via upload
    This is a simple Java-based alarm clock application that allows users to set an alarm by entering a time in HH:MM:SS format.
    
    When the program runs:
    
    The user is prompted to enter a desired alarm time.
    
    If the entered time has already passed, the alarm triggers immediately and plays the selected audio file.
    
    If the entered time is in the future, the program continuously monitors the system clock and plays the alarm sound exactly when the specified time is reached.

---
+++ First_One/src/Main.java
@@ -0,0 +1,33 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // Variables
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filePath = "Vem_Vem_LOW.wav";
+
+        while (alarmTime == null){
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            }
+            catch (DateTimeParseException e){
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
+
+
+    }
+}
